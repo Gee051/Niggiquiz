9bd1661c78c5960035b4cf01c75a91dfb019c200
Grace <otuaghagrace@gmail.com>
2024-08-02 20:37:26 +0100

    first commit

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -1,17 +1,10 @@
-"use client"
-import React, { Suspense, useState } from 'react'
+import React, { Suspense } from 'react'
 import QuestionsContent from '../components/QuestionsContent';
 
 export default function Questions() {
-  const [key, setKey] = useState(0);
-
-  const handleRestart = () => {
-    setKey(prevKey => prevKey + 1);
-  };
-
   return (
     <Suspense fallback={<div className='p-3'>Loading...</div>}>
-      <QuestionsContent key={key} onRestart={handleRestart} />
-    </Suspense>
-  );
+    <QuestionsContent/>
+  </Suspense>
+);
 }
